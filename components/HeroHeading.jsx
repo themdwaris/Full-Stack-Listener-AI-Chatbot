3de6404c2725629fce9823aaaa3e18f3a8dbf894
Mohammad Waris <mdtech9900@gmail.com>
2026-03-01 16:01:50 +0530

    Listener-ai

--- components/HeroHeading.jsx
+++ components/HeroHeading.jsx
@@ -9,9 +9,22 @@ const HeroHeading = () => {
     <div className="flex items-center gap-2.5">
       <SiGooglegemini size={28} />
       <span className="text-xl md:text-2xl">
-        {user && user?.name ? `Hey` : <span className="bg-gradient-to-r from-blue-500 via-purple-500 via-pink-500 to-yellow-400 bg-clip-text text-transparent">Listener</span>}&nbsp;
-        <span className="text-xl font-medium md:text-2xl">
-          {user && user?.name ? <span className="bg-gradient-to-r from-blue-500 via-purple-500 via-pink-500 to-yellow-400 bg-clip-text text-transparent">{user?.name}</span> : "Ai"}
+        {user && user?.name ? (
+          `Hello, `
+        ) : (
+          <span className="bg-gradient-to-r from-blue-500 via-purple-500 via-pink-500 to-yellow-400 bg-clip-text text-transparent">
+            Listener
+          </span>
+        )}
+        &nbsp;
+        <span className="text-xl md:text-2xl">
+          {user && user?.name ? (
+            <span className="font-medium bg-gradient-to-r from-blue-500 via-purple-500 via-pink-500 to-yellow-400 bg-clip-text text-transparent">
+              {user?.name}
+            </span>
+          ) : (
+            <span className="font-normal">Ai</span>
+          )}
         </span>
       </span>
     </div>
